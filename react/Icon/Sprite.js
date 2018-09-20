d7c9e9306c86a5c4a09d4c2f0590594ba923c334
Patrick Browne <pt.browne@gmail.com>
2018-09-20 17:31:12 +0200

    feat: Add icon sprite
    BREAKING CHANGE: Icon sprite is to be inserted by
    the user. svg-sprite-loader is no longer used.

---
+++ react/Icon/Sprite.js
@@ -0,0 +1,8 @@
+import IconSprintContent from './icons-sprite'
+import React from 'react'
+
+const IconSprite = () => {
+  return <div dangerouslySetInnerHTML={{ __html: IconSprintContent }} />
+}
+
+export default IconSprite
